fdc5ee16b253d30ca28b875381d764233b61437d
Ed Sungik Choi <sungik.dev@gmail.com>
2021-09-26 19:22:55 +0900

    React Router 세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
 /* External dependencies */
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 /* Internal dependencies */
 import Counter from 'Features/counter/Counter';
 
 function App() {
   return (
-    <div>
-      <Counter />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Counter />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
