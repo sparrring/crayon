3a60d16f691910c40dfd567cead54c1fe762d9b8
Ed Sungik Choi <sungik.dev@gmail.com>
2021-10-03 20:36:06 +0900

    슬라이스 화살표 함수를 기명 함수로 변경

--- client/src/features/register/registerSlice.ts
+++ client/src/features/register/registerSlice.ts
@@ -12,7 +12,7 @@ export const registerSlice = createSlice({
   name: 'register',
   initialState,
   reducers: {
-    setName: (state, action: PayloadAction<string>) => {
+    setName(state, action: PayloadAction<string>) {
       state.name = action.payload;
     },
   },
